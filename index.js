4c3181f71238150f882e780ecc838d913a81e260
Juan <juhernandezm@unal.edu.co>
2023-02-16 13:45:41 -0500

    se finaliza estilos, interactividad y readme

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const btn=document.querySelector('btn')
-const rateCard= document.querySelector('rateCard')
-const thanksCard= document.querySelector('rateCard')
+const btn=document.querySelector('.btn')
+const rateCard= document.querySelector('.rateCard')
+const thanksCard= document.querySelector('.thanksCard')
+const rateSpan=document.querySelector('.selectedScore')
+const s1=document.querySelector('#s1')
+const s2=document.querySelector('#s2')
+const s3=document.querySelector('#s3')
+const s4=document.querySelector('#s4')
+const s5=document.querySelector('#s5')
+let options=[]
+options.push(s1, s2, s3, s4, s5)
+let userRateObject
+let userRate
+
 
 btn.addEventListener('click', ()=>{
     thanksCard.classList.remove('inactive')
-    rateCard.clasList.add('inactive')
+    rateCard.classList.add('inactive')
+    selectRate()
 })
+
+function selectRate(){
+    options.forEach((option)=>{
+    if(option.checked){
+        userRateObject=option
+    }})
+    userRate=userRateObject.name
+    writeRate()
+}
+
+function writeRate(){
+    rateSpan.innerHTML=`${userRate} `
+}
